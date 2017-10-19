623a5fcca9dff95dd04a7bdfec36f3dfb0cfb284
elijah <elijahe.joseph@outlook.com>
2017-10-18 22:25:22 -0700

    Added stuff

---
+++ CarApp/CarApp/Data/CarContext.cs
@@ -0,0 +1,22 @@
+﻿using CarApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class CarContext : DbContext
+    {
+        //This class creates the database properties from the models in the code. This means that we are going
+        //to use a code first approach to write the application.
+        public CarContext(DbContextOptions<CarContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().ToTable("Car");
+        }
+    }
+}
---
+++ CarApp/CarApp/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarApp.Models
+{
+    public class Car
+    {
+        //Using a code-first approach, we are creating a car model which
+        //the entities will be structured around and then put into the 
+        //database through Entity Framework.
+
+        public int Year;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+        public double MPG { get; set; }
+
+    }
+}
